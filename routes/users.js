ac5bd6ddb95375e45ea5ec36da54a31eaab6156e
Raphael Gomes <radago@hotmail.com.br>
2019-10-23 23:27:01 -0300

    Adição routes/users, rota somente para adms

---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const Router = require("express").Router();
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+const jwtSecret = "asdasdqwecxv";
+
+Router.use(async (req, res, next) => {
+  const token =
+    req.headers["x-access-token"] || req.body.token || req.query.token;
+  if (token) {
+    try {
+      const payload = jwt.verify(token, jwtSecret);
+      if (payload.roles.indexOf("admin") >= 0) next();
+      else res.send({ success: false, message: "without authorization" });
+    } catch (e) {
+      res.send({ success: false, message: "without authorization" });
+    }
+  } else res.send({ success: false, message: "without token" });
+});
+
+Router.get("/", async (req, res) => {
+  const users = await User.find({});
+  res.send(users);
+});
+
+module.exports = Router;
