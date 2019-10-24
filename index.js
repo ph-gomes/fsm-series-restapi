5e3a2f54de32b886922a762771a381db79cecbad
Raphael Gomes <radago@hotmail.com.br>
2019-10-23 23:28:44 -0300

    Adição de novas rotas e criação automatica de 2 usuarios

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const series = require("./routes/series");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+
+const User = require("./models/user");
 
 const port = process.env.PORT || 3000;
 const mongo = process.env.MONGODB || "mongodb://localhost/minhas-series-rest";
@@ -12,13 +16,33 @@ const app = express();
 
 app.use(bodyParser.json({ extended: true }));
 
+const createInitialUsers = async () => {
+  const total = await User.countDocuments({});
+  if (total === 0) {
+    const admin = new User({
+      username: "admin",
+      password: "1234",
+      roles: ["restrict", "admin"]
+    });
+    await admin.save();
+
+    const user = new User({
+      username: "user",
+      password: "1234",
+      roles: ["restrict"]
+    });
+    await user.save();
+  }
+};
+
 app.use("/series", series);
+app.use("/users", users);
+app.use("/auth", auth);
 
 mongoose
   .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Listening on port ${3000}`);
-    });
+    createInitialUsers();
+    app.listen(port, () => console.log(`Listening on port ${3000}`));
   })
   .catch(e => console.log("can't initialize the server"));
